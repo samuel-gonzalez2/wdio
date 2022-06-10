4a10c8075795dc77dd20722fc616e1dc4dff26c2
samuel-gonzalez2 <samuel.gonzalez@enroutesystems.com>
2022-06-09 22:19:14 -0500

    understandig differences between old and modern sintax of wdio

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,4 +1,4 @@
-describe('My Login application', () => {
+/* describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await browser.url(`https://the-internet.herokuapp.com/login`);
 
@@ -10,5 +10,30 @@ describe('My Login application', () => {
         await expect($('#flash')).toHaveTextContaining(
             'You logged into a secure area!');
     });
-});
+}); */
 
+describe('My first test suite', () => {
+    it('my first wdio test', async () => {
+        //TODO
+        let myUrl = "https://www.example.com";
+        let smallPause = 2000;
+        let largePause = 5000;
+
+        await browser.url(myUrl);
+        await browser.pause(smallPause);
+
+/*      await browser.url("https://www.saucedemo.com/");
+        await browser.pause(largePause); */
+
+        //In procees of being deprecated
+        let pageTitle = await browser.getTitle();
+        let pageUrl = await browser.getUrl();
+
+/*         expect (pageTitle).toContain("Example Domain");
+        expect (pageUrl).toContain("example.com"); */
+
+        //Modern way of doing it
+        await expect(browser).toHaveTitleContaining("Example Domain");
+        await expect(browser).toHaveUrlContaining("example.com");
+    });
+});
