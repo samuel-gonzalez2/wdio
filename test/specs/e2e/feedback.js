4d10d200ecd51eefbba8cb4cb2450f31fbea04cc
samuel-gonzalez2 <samuel.gonzalez@enroutesystems.com>
2022-06-17 17:50:39 -0500

    [CREATED] Feedback Form

---
+++ test/specs/e2e/feedback.js
@@ -0,0 +1,19 @@
+describe('Feedback Form', () => {
+    it('Should submit feedback form', async () => {
+        const name = 'Test User';
+        const email = 'test@test.com';
+        const subject = 'Test Subject';
+        const message = 'Test Message';
+
+        await browser.url('http://zero.webappsecurity.com/index.html');
+        await (await $('#feedback')).click();
+
+        await (await $('#name')).setValue(name);
+        await (await $('#email')).setValue(email);
+        await (await $('#subject')).setValue(subject);
+        await (await $('#comment')).setValue(message);
+        await (await $('input[type="submit"]')).click();
+
+        await expect(browser).toHaveUrlContaining('/sendFeedback.html');
+})
+})
