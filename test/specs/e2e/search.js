76558c13e40495ab459c94cce70051cfc3e7cd18
samuel-gonzalez2 <samuel.gonzalez@enroutesystems.com>
2022-06-17 17:28:01 -0500

    [CREATED] Search feature

---
+++ test/specs/e2e/search.js
@@ -0,0 +1,11 @@
+describe('Search Feature', () => {
+    it('Should search for values using the keyboard press', async () => {
+        await browser.url('http://zero.webappsecurity.com/index.html');
+        await (await $('#searchTerm')).waitForDisplayed();
+        await (await $('#searchTerm')).setValue('bank');
+        await browser.keys('Enter');
+
+        const results = await $('h2');
+        await expect(results).toHaveTextContaining('Search Results');
+    })
+})
